df7fe4ebbdb2878355a5dd558039e252b14e07b3
GaidamakUA <jMazurevich@gmail.com>
2014-11-30 00:11:38 +0200

    Search on list is functional

--- app/build/generated/source/r/debug/com/example/gaidamak/mathcatalog/R.java
+++ app/build/generated/source/r/debug/com/example/gaidamak/mathcatalog/R.java
@@ -20,8 +20,8 @@ containing a value of this type.
         public static final int floatLabelColor=0x7f010000;
     }
     public static final class dimen {
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int background_v2=0x7f020000;
@@ -31,24 +31,24 @@ containing a value of this type.
         public static final int vertical_background=0x7f020004;
     }
     public static final class id {
-        public static final int action_cancel=0x7f05000f;
-        public static final int action_edit=0x7f050011;
-        public static final int action_save=0x7f050010;
-        public static final int addItem=0x7f05000e;
-        public static final int container=0x7f050002;
-        public static final int description_edittext=0x7f050007;
-        public static final int description_textview=0x7f05000b;
-        public static final int edit_text=0x7f050000;
-        public static final int float_label=0x7f050001;
-        public static final int formula_edittext=0x7f050006;
-        public static final int formula_textview=0x7f05000a;
-        public static final int search=0x7f05000d;
-        public static final int search_edittext=0x7f050009;
-        public static final int tags_edittext=0x7f050008;
-        public static final int tags_textview=0x7f05000c;
-        public static final int term_formula=0x7f050004;
-        public static final int term_name=0x7f050003;
-        public static final int title_edittext=0x7f050005;
+        public static final int action_cancel=0x7f06000f;
+        public static final int action_edit=0x7f060011;
+        public static final int action_save=0x7f060010;
+        public static final int addItem=0x7f06000e;
+        public static final int container=0x7f060002;
+        public static final int description_edittext=0x7f060007;
+        public static final int description_textview=0x7f06000b;
+        public static final int edit_text=0x7f060000;
+        public static final int float_label=0x7f060001;
+        public static final int formula_edittext=0x7f060006;
+        public static final int formula_textview=0x7f06000a;
+        public static final int search=0x7f06000d;
+        public static final int search_edittext=0x7f060009;
+        public static final int tags_edittext=0x7f060008;
+        public static final int tags_textview=0x7f06000c;
+        public static final int term_formula=0x7f060004;
+        public static final int term_name=0x7f060003;
+        public static final int title_edittext=0x7f060005;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -61,28 +61,27 @@ containing a value of this type.
         public static final int fragment_view_term=0x7f030007;
     }
     public static final class menu {
-        public static final int add_item=0x7f080000;
-        public static final int fragment_edit_term_menu=0x7f080001;
-        public static final int fragment_view_term_menu=0x7f080002;
+        public static final int add_item=0x7f090000;
+        public static final int fragment_edit_term_menu=0x7f090001;
+        public static final int fragment_view_term_menu=0x7f090002;
     }
     public static final class string {
-        public static final int action_add=0x7f060000;
-        public static final int app_name=0x7f060001;
-        public static final int description_hint=0x7f060002;
-        public static final int formula_hint=0x7f060003;
-        public static final int hello_blank_fragment=0x7f060004;
-        public static final int math_term_hint=0x7f060005;
-        public static final int tags_hint=0x7f060006;
+        public static final int action_add=0x7f070000;
+        public static final int app_name=0x7f070001;
+        public static final int description_hint=0x7f070002;
+        public static final int formula_hint=0x7f070003;
+        public static final int hello_blank_fragment=0x7f070004;
+        public static final int math_term_hint=0x7f070005;
+        public static final int tags_hint=0x7f070006;
     }
     public static final class style {
-        /** <item name="android:textColor">#FFF</item>
-<item name="android:textColorPrimary">#FFF</item>
-<item name="android:textColorHint">#b4b4b4</item>
-         */
-        public static final int AppTheme=0x7f070000;
+        public static final int AppTheme=0x7f080000;
         /** <item name="android:background">#8a4900</item>
          */
-        public static final int MyActionBar=0x7f070001;
+        public static final int MyActionBar=0x7f080001;
+    }
+    public static final class xml {
+        public static final int searchable=0x7f040000;
     }
     public static final class styleable {
         /** Attributes that can be used with a FloatLabel.
